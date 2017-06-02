7ad5d051f25a0de3391f9de1395ddf8a3d0b3ad4
Kareem <Kareem@KAREEM-PC>
2017-06-03 00:10:29 +0200

    Add BaseNode and TerminalNode classes

---
+++ FlowchartConverter/Nodes/BaseNode.cs
@@ -0,0 +1,218 @@
+﻿using Crainiate.Diagramming;
+using Crainiate.Diagramming.Flowcharting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlowchartConverter.Nodes
+{
+    class BaseNode : Crainiate.Diagramming.OnShapeClickListener
+    {
+        private static MainForm form;
+        private Form dialog;
+        private bool toBeRemoved = false;
+
+        private Shape shape;
+        private BaseNode parentNode;
+        private FlowchartStencil stencil;
+
+        private String statement;
+        private String shapeTag;
+        private String connectorTag;
+
+        protected PointF nodeLocation;
+
+        private static int counter;
+
+        public static MainForm Form
+        {
+            get
+            {
+                return form;
+            }
+
+            set
+            {
+                form = value;
+            }
+        }
+
+        public Form Dialog
+        {
+            get
+            {
+                return dialog;
+            }
+
+            set
+            {
+                dialog = value;
+            }
+        }
+
+        public bool ToBeRemoved
+        {
+            get
+            {
+                return toBeRemoved;
+            }
+
+            set
+            {
+                toBeRemoved = value;
+            }
+        }
+
+        public Shape Shape
+        {
+            get
+            {
+                return shape;
+            }
+        }
+
+        public BaseNode ParentNode
+        {
+            get
+            {
+                return parentNode;
+            }
+
+            set
+            {
+                parentNode = value;
+            }
+        }
+
+        public FlowchartStencil Stencil
+        {
+            get
+            {
+                return stencil;
+            }
+
+            set
+            {
+                stencil = value;
+            }
+        }
+
+        public string Statement
+        {
+            get
+            {
+                return statement;
+            }
+
+            set
+            {
+                statement = value;
+                showStatment();
+            }
+        }
+
+        public string ShapeTag
+        {
+            get
+            {
+                return shapeTag;
+            }
+
+            set
+            {
+                shapeTag = value;
+            }
+        }
+
+        public string ConnectorTag
+        {
+            get
+            {
+                return connectorTag;
+            }
+
+            set
+            {
+                connectorTag = value;
+            }
+        }
+
+        public virtual PointF NodeLocation
+        {
+            get
+            {
+                return nodeLocation;
+            }
+
+            set
+            {
+                nodeLocation = value;
+                connectedShape().Location = value;
+            }
+        }
+
+        public string Name { get; internal set; }
+
+        public BaseNode()
+        {
+            this.shape = new Shape();
+            this.Shape.Label = new Crainiate.Diagramming.Label();
+            this.Shape.Label.Color = Color.White;
+            this.Shape.KeepAspect = false;
+            this.Shape.ShapeListener = this;
+            this.Shape.AllowMove = Shape.AllowScale = Shape.AllowRotate = Shape.AllowSnap = false;
+            this.Shape.Size = new SizeF(80, 50);
+            this.Shape.KeepAspect = false;
+            this.Shape.Label.Color = Color.White;
+            this.Stencil = (FlowchartStencil)Singleton.Instance.GetStencil(typeof(FlowchartStencil));
+            counter = counter + 1;
+            this.ShapeTag = "Shape_" + counter.ToString();
+            this.ConnectorTag = "Connector_" + counter.ToString();
+            
+        }
+
+        public BaseNode(PointF location) : this()
+        {
+            NodeLocation = location;
+        }
+
+        protected virtual void showStatment()
+        {
+            if (!String.IsNullOrEmpty(Statement))
+                setText(Statement);
+        }
+
+        public virtual Shape connectedShape()
+        {
+            return Shape;
+        }
+
+        public virtual void setText(String label)
+        {
+            Shape.Label = new Crainiate.Diagramming.Label(label);
+            SizeF size;
+            using (Graphics g = Graphics.FromHwnd(IntPtr.Zero))
+            {
+                size = g.MeasureString(label, Shape.Label.Font);
+            }
+
+            float oldWidth = Shape.Size.Width;
+            Shape.Size = new SizeF(size.Width + 50, Shape.Size.Height);
+            Shape.Label.Color = Color.White;
+        }
+
+        public virtual void addRemoveFlag(bool v)
+        {
+            toBeRemoved = v;
+        }
+
+        public virtual void onShapeClicked()
+        {
+            //To be implemented
+        }
+    }
+}
---
+++ FlowchartConverter/Nodes/TerminalNode.cs
@@ -0,0 +1,37 @@
+﻿using Crainiate.Diagramming.Flowcharting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowchartConverter.Nodes
+{
+    class TerminalNode : BaseNode
+    {
+        public enum TerminalType { Start, End }
+
+        public TerminalNode(TerminalType termType)
+        {
+            base.Shape.StencilItem = Stencil[FlowchartStencilType.Terminator];
+            base.Shape.BackColor = Color.Magenta;
+            base.Shape.StencilItem.GradientColor = Color.Black;
+
+            if (termType == TerminalType.Start) {
+                base.Statement = "Start";
+                base.setText("Start");
+                base.Name = "Start";
+                base.NodeLocation = new PointF(80, 10);
+                base.ParentNode = this;
+            }
+            else {
+                base.Statement = "End";
+                base.setText("End");
+                base.Name = "End";
+            }
+        }
+
+        public override void onShapeClicked() { }
+    }
+}
