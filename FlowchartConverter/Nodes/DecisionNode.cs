cfdca61d2825f1e8dbfe41bc5354fddce3d16fc5
Kareem <Kareem@KAREEM-PC>
2017-06-03 19:38:42 +0200

    Add DecisionNode and WhileNode classes

---
+++ FlowchartConverter/Nodes/DecisionNode.cs
@@ -0,0 +1,72 @@
+﻿using Crainiate.Diagramming.Flowcharting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowchartConverter.Nodes
+{
+    class DecisionNode : BaseNode
+    {
+        private HolderNode trueNode;
+        private HolderNode backNode;
+        protected ConnectorNode trueConnector;
+
+        private readonly string holderTag;
+        private readonly string backConnectorTag;
+        private readonly string trueConnectorTag;
+
+
+        public HolderNode TrueNode
+        {
+            get
+            {
+                return trueNode;
+            }
+
+            set
+            {
+                trueNode = value;
+            }
+        }
+
+        public HolderNode BackNode
+        {
+            get
+            {
+                return backNode;
+            }
+
+            set
+            {
+                backNode = value;
+            }
+        }
+
+        public ConnectorNode TrueConnector
+        {
+            get
+            {
+                return trueConnector;
+            }
+
+            set
+            {
+                trueConnector = value;
+            }
+        }
+
+        public DecisionNode()
+        {
+            base.Shape.StencilItem = Stencil[FlowchartStencilType.Preparation];
+            base.Shape.BackColor = System.Drawing.ColorTranslator.FromHtml("#e06000");
+            base.Shape.GradientColor = Color.Black;
+
+            this.holderTag = base.ShapeTag + " holder";
+            this.backConnectorTag = base.ShapeTag + " backConnector";
+            this.trueConnectorTag = base.ShapeTag + " trueConnector";
+        }
+    }
+}
---
+++ FlowchartConverter/Nodes/WhileNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowchartConverter.Nodes
+{
+    class WhileNode : DecisionNode
+    {
+        public WhileNode()
+        {
+            base.Name = "While";
+            base.setText("While");
+        }
+
+        public override void onShapeClicked()
+        {
+            //To be implemented 
+        }
+    }
+}
