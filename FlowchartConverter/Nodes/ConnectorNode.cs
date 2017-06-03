654c304d6a3ac8294dfb430e50da1da14db79279
Kareem <Kareem@KAREEM-PC>
2017-06-03 15:45:41 +0200

    Add ConnectorNode and HolderNode classes

---
+++ FlowchartConverter/Nodes/ConnectorNode.cs
@@ -0,0 +1,95 @@
+﻿using Crainiate.Diagramming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowchartConverter.Nodes
+{
+    class ConnectorNode : Crainiate.Diagramming.OnShapeClickListener
+    {
+        private BaseNode startNode;
+        private BaseNode endNode;
+        private Connector connector = new Connector();
+        private bool selectable = true;
+
+        public Connector Connector
+        {
+            get
+            {
+                return connector;
+            }
+
+            set
+            {
+                connector = value;
+            }
+        }
+
+        public BaseNode StartNode
+        {
+            get
+            {
+                return startNode;
+            }
+
+            set
+            {
+                connector.Start.Shape = value.connectedShape();
+                startNode = value;
+            }
+        }
+
+        public BaseNode EndNode
+        {
+            get
+            {
+                return endNode;
+            }
+
+            set
+            {
+                endNode = value;
+                connector.End.Shape = value.connectedShape();
+
+            }
+        }
+
+        public bool Selectable
+        {
+            get
+            {
+                return selectable;
+            }
+
+            set
+            {
+                selectable = value;
+            }
+        }
+
+        public ConnectorNode(BaseNode startNode)
+        {
+            
+            this.startNode = startNode;
+            this.connector.Start.Shape = startNode.Shape;
+            this.connector.OnShapeSelectedListener = this;
+            this.connector.End.Marker = new Arrow();
+            this.connector.AllowMove = true;
+            this.connector.Avoid = connector.Jump = false;
+            this.connector.SetOrder(1);
+            this.connector.DrawSelected = true;
+        }
+
+        public void setEndNode(BaseNode endNode)
+        {
+            this.endNode = endNode;
+        }
+
+        public void onShapeClicked()
+        {
+            //To be implemented
+        }
+    }
+}
---
+++ FlowchartConverter/Nodes/HolderNode.cs
@@ -0,0 +1,26 @@
+﻿using Crainiate.Diagramming.Flowcharting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowchartConverter.Nodes
+{
+    class HolderNode : BaseNode
+    {
+        private static int hCounter = 1;
+
+        public HolderNode(BaseNode parentNode)
+        {
+            base.ParentNode = parentNode;
+            base.Shape.StencilItem = Stencil[FlowchartStencilType.Connector];
+            base.Shape.MinimumSize = new System.Drawing.SizeF(15, 15);
+            base.Shape.BackColor = System.Drawing.Color.White;
+            base.Shape.Size = new System.Drawing.SizeF(15, 15);
+            base.Shape.GradientColor = System.Drawing.Color.White;
+            base.ShapeTag = "Shape_Holder" + hCounter.ToString();
+            hCounter++;
+        }
+    }
+}
