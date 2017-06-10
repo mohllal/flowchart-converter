c8747c2615161de4275f0b4fffc81dd33590006c
Kareem <Kareem@KAREEM-PC>
2017-06-11 01:37:05 +0200

    Add AssignNode and DeclareNode classes

---
+++ FlowchartConverter/Nodes/AssignNode.cs
@@ -0,0 +1,27 @@
+﻿using Crainiate.Diagramming.Flowcharting;
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FlowchartConverter.Dialogs;
+
+namespace FlowchartConverter.Nodes
+{
+    class AssignNode : BaseNode
+    {       
+        public AssignNode()
+        {
+            base.Name = "Assign";
+            base.Shape.StencilItem = Stencil[FlowchartStencilType.Default];
+            base.Shape.BackColor = System.Drawing.ColorTranslator.FromHtml("#fdfd80");
+            base.Shape.GradientColor = Color.Black;
+            base.setText("Assign");
+        }
+
+        public override void onShapeClicked() { }
+    }
+}
---
+++ FlowchartConverter/Nodes/DeclareNode.cs
@@ -0,0 +1,32 @@
+﻿using Crainiate.Diagramming.Flowcharting;
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FlowchartConverter.Dialogs;
+
+namespace FlowchartConverter.Nodes
+{
+    class DeclareNode : BaseNode
+    {
+        public DeclareNode()
+        {
+            base.Name = "Declare";
+            base.Shape.StencilItem = Stencil[FlowchartStencilType.InternalStorage];
+            base.Shape.BackColor = System.Drawing.ColorTranslator.FromHtml("#e3810c");
+            base.Shape.GradientColor = Color.Black;
+            base.setText("Declare");
+        }
+
+        protected override void showStatment()
+        {
+            base.setText(Statement);
+        }
+
+        public override void onShapeClicked() {}
+    }
+}
