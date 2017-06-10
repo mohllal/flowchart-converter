f4dba7c4a137e121a94978e94bad77f22a2ccaf8
Kareem <Kareem@KAREEM-PC>
2017-06-10 02:50:19 +0200

    Add InputNode and OutputNode classes

---
+++ FlowchartConverter/Nodes/InputNode.cs
@@ -0,0 +1,33 @@
+﻿using Crainiate.Diagramming;
+using Crainiate.Diagramming.Flowcharting;
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FlowchartConverter.Dialogs;
+
+namespace FlowchartConverter.Nodes
+{
+    class InputNode : BaseNode
+    {
+        public InputNode()
+        {
+            base.Name = "Input";
+            base.Shape.StencilItem = Stencil[FlowchartStencilType.Data];
+            base.Shape.BackColor = System.Drawing.ColorTranslator.FromHtml("#0040a0");
+            base.Shape.GradientColor = Color.Black;
+            base.setText("Input");
+        }
+
+        protected override void showStatment()
+        {
+            base.setText("Read " + Statement);
+        }
+
+        public override void onShapeClicked() { }
+    }
+}
---
+++ FlowchartConverter/Nodes/OutputNode.cs
@@ -0,0 +1,32 @@
+﻿using Crainiate.Diagramming.Flowcharting;
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FlowchartConverter.Dialogs;
+
+namespace FlowchartConverter.Nodes
+{
+    class OutputNode : BaseNode
+    {
+        public OutputNode()
+        {
+            base.Name = "Output";
+            base.Shape.StencilItem = Stencil[FlowchartStencilType.Data];
+            base.Shape.StencilItem.BackColor = System.Drawing.Color.Green;
+            base.Shape.StencilItem.GradientColor = Color.Black;
+            base.setText("Output");
+        }
+
+        protected override void showStatment()
+        {
+            base.setText("Print " + Statement);
+        }
+
+        public override void onShapeClicked() { }
+    }
+}
