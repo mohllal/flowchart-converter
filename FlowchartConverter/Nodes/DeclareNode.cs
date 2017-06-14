56630f874c141c8544166c88532b526df89b33ac
Kareem <Kareem@KAREEM-PC>
2017-06-14 16:22:10 +0200

    Add onShapeClicked to DeclareNode class

--- FlowchartConverter/Nodes/DeclareNode.cs
+++ FlowchartConverter/Nodes/DeclareNode.cs
@@ -13,6 +13,83 @@ namespace FlowchartConverter.Nodes
 {
     class DeclareNode : BaseNode
     {
+        public class Variable
+        {
+            private enum Data_Type { INTEGER, STRING, REAL, BOOLEAN, CHAR }
+
+            private String varName;
+            private Data_Type varType;
+            private bool single;
+            private int size;
+
+            public string VarName
+            {
+                get
+                {
+                    return varName;
+                }
+
+                set
+                {
+                    varName = value;
+                }
+            }
+
+            public Data_Type VarType
+            {
+                get
+                {
+                    return varType;
+                }
+
+                set
+                {
+                    varType = value;
+                }
+            }
+
+            public bool Single
+            {
+                get
+                {
+                    return single;
+                }
+
+                set
+                {
+                    single = value;
+                }
+            }
+
+            public int Size
+            {
+                get
+                {
+                    return size;
+                }
+
+                set
+                {
+                    size = value;
+                }
+            }
+        }
+
+        private Variable variable;
+
+        public Variable _Var
+        {
+            get
+            {
+                return variable;
+            }
+
+            set
+            {
+                variable = value;
+            }
+        }
+
         public DeclareNode()
         {
             base.Name = "Declare";
@@ -20,13 +97,77 @@ namespace FlowchartConverter.Nodes
             base.Shape.BackColor = System.Drawing.ColorTranslator.FromHtml("#e3810c");
             base.Shape.GradientColor = Color.Black;
             base.setText("Declare");
+
+            this.variable = new Variable();
         }
 
         protected override void showStatment()
         {
-            base.setText(Statement);
+            base.setText(base.Statement);
         }
 
-        public override void onShapeClicked() {}
+        public override void onShapeClicked()
+        {
+            base.onShapeClicked();
+            if (Shape.Selected)
+            {
+                TextBox textBox = new TextBox();
+                textBox.Location = new Point((int)Shape.Location.X, (int)Shape.Location.Y);
+                textBox.Width = (int)Shape.Width;
+                DeclareDialog db = new DeclareDialog();
+                DialogResult dr = db.ShowDialog();
+                if (dr == DialogResult.OK)
+                {
+                    if (String.IsNullOrEmpty(db.DeclareDataType) || String.IsNullOrEmpty(db.DeclareVariableType)
+                        || String.IsNullOrWhiteSpace(db.DeclareDataType) || String.IsNullOrWhiteSpace(db.DeclareVariableType)
+                        || String.IsNullOrEmpty(db.DeclareVariable) || String.IsNullOrWhiteSpace(db.DeclareVariable))
+                    {
+                        MessageBox.Show("You must enter a valid declare expression");
+                        return;
+                    }
+
+                    if (db.DeclareVariableType.Equals("Array") && (String.IsNullOrEmpty(db.DeclareArraySize) || String.IsNullOrWhiteSpace(db.DeclareArraySize)))
+                    {
+                        MessageBox.Show("You must enter a valid declare expression");
+                        return;
+                    }
+                    this.initializeVariable(db);
+                    this.makeStatment();
+                }
+            }
+            base.Shape.Selected = false;
+        }
+
+        private void makeStatment()
+        {
+            if (this.variable.Single)
+                base.Statement = this.variable.VarType + "  " + this.variable.VarName;
+            else
+                base.Statement = this.variable.VarType + " Array " + this.variable.VarName + "[" + this.variable.Size + "]";
+
+        }
+
+        private void initializeVariable(DeclareDialog db)
+        {
+            this.variable.VarName = db.DeclareVariable;
+            if (db.DeclareDataType.Equals("Integer"))
+                this.variable.VarType = Variable.Data_Type.INTEGER;
+            else if (db.DeclareDataType.Equals("Float"))
+                this.variable.VarType = Variable.Data_Type.REAL;
+            else if (db.DeclareDataType.Equals("Bool"))
+                this.variable.VarType = Variable.Data_Type.BOOLEAN;
+            else
+                this.variable.VarType = Variable.Data_Type.STRING;
+
+
+            if (db.DeclareVariableType.Equals("Array"))
+            {
+                this.variable.Single = false;
+
+                this.variable.Size = Int32.Parse(db.DeclareArraySize);
+            }
+            else
+                this.variable.Single = true;
+        }
     }
 }
